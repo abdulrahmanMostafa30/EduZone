0a0494963e4ebc5f1fd6a6bf6c4a2155fb741635
Kholoud Mohamed <kholoudmohamedf6@gmail.com>
2023-06-05 18:39:53 +0300

    header added, have a look !

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SliderComponent } from './slider/slider.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SliderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
