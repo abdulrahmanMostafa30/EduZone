2002f32299699ea635e1f908b7cdb7cfac3f2f99
abdulRahmanMostafa30 <3bdo.mostafa@protonmail.com>
2023-06-08 19:02:25 +0300

    Bootstrap with Angular

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -13,6 +13,9 @@ import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { CheckoutComponent } from './checkout/checkout.component';
 import { CoursesComponent } from './profile/courses/courses.component';
 import { FooterComponent } from './footer/footer.component';
+import { AddCourseComponent } from './profile/courses/add-course/add-course.component';
+import { InstructorComponent } from './instructor/instructor.component';
+import { StudentComponent } from './student/student.component';
 
 
 @NgModule({
@@ -25,7 +28,10 @@ import { FooterComponent } from './footer/footer.component';
     ShoppingCartComponent,
     CheckoutComponent,
     CoursesComponent,
-    FooterComponent
+    FooterComponent,
+    AddCourseComponent,
+    InstructorComponent,
+    StudentComponent
   ],
   imports: [
     BrowserModule,
