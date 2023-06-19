2597e8e290be65aba491863eacbd301d56c1e9de
Lamiaa Selim <lamiaaselim1896@gmail.com>
2023-06-19 19:29:20 +0300

    ui-lamiaa-done

--- client/src/app/sign-up/sign-up.component.ts
+++ client/src/app/sign-up/sign-up.component.ts
@@ -5,7 +5,17 @@ import { Component } from '@angular/core';
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss']
 })
-
 export class SignUpComponent {
+
+  displaySelectedImage(event: any) {
+    if (event.target.files && event.target.files[0]) {
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        const imgElement = document.getElementById('photo') as HTMLImageElement;
+        imgElement.src = e.target.result;
+      };
+      reader.readAsDataURL(event.target.files[0]);
+    }
+  }
 
 }
