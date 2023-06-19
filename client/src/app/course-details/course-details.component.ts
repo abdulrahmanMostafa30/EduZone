cb2968ac059710695ee5c69fd6e31b37e12a0786
Lamiaa Selim <lamiaaselim1896@gmail.com>
2023-06-19 04:12:13 +0300

    upload course details modify

--- client/src/app/course-details/course-details.component.ts
+++ client/src/app/course-details/course-details.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component ,ViewChild ,ElementRef ,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-course-details',
   templateUrl: './course-details.component.html',
   styleUrls: ['./course-details.component.scss']
 })
-export class CourseDetailsComponent {
+export class CourseDetailsComponent implements OnInit {
 
+  @ViewChild('mycomment', { static: false }) myCommentInput!: ElementRef;
+  comments: string[] = [];
+  onclick() {
+    const comment = this.myCommentInput.nativeElement.value;
+    this.comments.push(comment);
+    // // Store the comments in localStorage
+    // localStorage.setItem('comments', JSON.stringify(this.comments));
+    // Clear the input after adding the comment
+    this.myCommentInput.nativeElement.value = '';
+  }
+  videoUrls: string[] = [
+    "https://www.youtube.com/embed/RHsuArdyZUI",
+    "https://www.youtube.com/embed/ogUfVTA5AlA",
+    "https://www.youtube.com/embed/Rj5MXsPslVo",
+    "https://www.youtube.com/embed/ogUfVTA5AlA",
+    "https://www.youtube.com/embed/RHsuArdyZUI",
+    "https://www.youtube.com/embed/ogUfVTA5AlA",
+    "https://www.youtube.com/embed/RHsuArdyZUI",
+    "https://www.youtube.com/embed/ogUfVTA5AlA",
+    "https://www.youtube.com/embed/RHsuArdyZUI",
+    "https://www.youtube.com/embed/ogUfVTA5AlA",
+    "https://www.youtube.com/embed/RHsuArdyZUI",
+    "https://www.youtube.com/embed/ogUfVTA5AlA"
+
+    // url of video
+  ];
+  currentVideoUrl: string = this.videoUrls[0]; //  Url of Default video
+
+  displayVideo(videoIndex: number) {
+    if (videoIndex >= 0 && videoIndex < this.videoUrls.length) {
+      this.currentVideoUrl = this.videoUrls[videoIndex];
+    }
+  }
+  ngOnInit() {
+    // // in case page refresh saved comments
+    // const storedComments = localStorage.getItem('comments');
+    // if (storedComments) {
+    //   this.comments = JSON.parse(storedComments);
+    // }
+  }
 }
