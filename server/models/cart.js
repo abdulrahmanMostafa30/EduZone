f9602786b6e8338ee6b8e2d4def9376a4ad93711
Lamiaa Selim <lamiaaselim1896@gmail.com>
2023-06-27 14:29:24 +0300

    cart1

---
+++ server/models/cart.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+let ItemSchema = new Schema({
+    courseId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Course",
+    },
+    tile: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    total: {
+        type: Number,
+        required: true,
+    }
+}, {
+    timestamps: true
+})
+const CartSchema = new Schema({
+    items: [ItemSchema],
+    subTotal: {
+        default: 0,
+        type: Number
+    }
+}, {
+    timestamps: true
+})
+module.exports = mongoose.model('cart', CartSchema);
