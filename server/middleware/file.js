a8cda1623ba5bea08e9f8664c6c6ef6399794bb0
abdulrahmanMostafa30 <3bdo.mostafa@protonmail.com>
2023-06-30 19:27:13 +0300

    error file

--- server/middleware/file.js
+++ server/middleware/file.js
@@ -9,18 +9,20 @@ const MIME_TYPE_MAP = {
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    const isValid = MIME_TYPE_MAP[file.mimetype];
+    // const isValid = MIME_TYPE_MAP[file.mimetype];
     let error = new Error("Invalid mime type");
-    if (isValid) {
-      error = null;
-    }
+    // if (isValid) {
+    //   error = null;
+    // }
     // cb(null, "images");
     cb(null, path.join("server/images"));
   },
   filename: (req, file, cb) => {
     const name = file.originalname.toLowerCase().split(" ").join("-");
-    const ext = MIME_TYPE_MAP[file.mimetype];
-    cb(null, name + "-" + Date.now() + "." + ext);
+    // const ext = MIME_TYPE_MAP[file.mimetype];
+    // cb(null, name + "-" + Date.now() + "." + ext);
+    cb(null, name + "-" + Date.now());
+
   },
 });
 
