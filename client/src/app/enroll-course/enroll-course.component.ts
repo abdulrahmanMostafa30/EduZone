1c96efd83e074645be1ba9ff3d45d289adecc1fa
abdulrahmanMostafa30 <3bdo.mostafa@protonmail.com>
2023-07-04 21:36:35 +0300

    isAuthenticated

--- client/src/app/enroll-course/enroll-course.component.ts
+++ client/src/app/enroll-course/enroll-course.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute, Router } from "@angular/router";
 import { CoursesService } from "../courses.service";
 import { CartService } from "../shopping-cart/cart.service";
 import { UserService } from "../profile/user.service";
+import { AuthService } from "../auth/auth.service";
 // import { CoursesService } from '../courses.service';
 
 @Component({
@@ -23,47 +24,65 @@ export class EnrollCourseComponent implements OnInit {
   isEnrolled = false;
   buttonLabel = "Enroll";
   user: any;
+  isAuthenticated = false;
   constructor(
     private route: ActivatedRoute,
     private coursesService: CoursesService,
     private router: Router,
     private cartService: CartService,
-    private userService: UserService
+    private userService: UserService,
+    private authService: AuthService
   ) {}
 
   ngOnInit() {
     this.id = this.route.snapshot.paramMap.get("id");
     this.coursesService.getCourseById(this.id).subscribe((data) => {
       this.course = data;
-      this.userService.getUserMe().subscribe((responseUser) => {
-        this.user = responseUser;
-        console.log(this.user.data.data.cart);
+      if (this.authService.getIsAuth()) {
+        this.isAuthenticated = true;
+      }
+      if (this.isAuthenticated) {
+        this.userService.getUserMe().subscribe((responseUser) => {
+          this.user = responseUser;
+          console.log(this.user.data.data.cart);
 
-        if (this.user.data.data.cart.includes(this.course._id)) {
-          this.isEnrolled = true;
-          this.buttonLabel = "Enrolled";
-          setTimeout(() => {
-            this.router.navigate(["course/enroll", this.course._id]);
-          }, 500);
-        }
-      });
+          if (this.user.data.data.cart.includes(this.course._id)) {
+            this.isEnrolled = true;
+            this.buttonLabel = "Enrolled";
+            setTimeout(() => {
+              this.router.navigate(["course/enroll", this.course._id]);
+            }, 500);
+          }
+        });
+      }
+      else{
+        this.buttonLabel = "Login to Enroll";
+      }
     });
   }
   enroll() {
-    this.isEnrolled = true;
-    this.buttonLabel = "Enrolled";
+    if(this.isAuthenticated){
+      this.isEnrolled = true;
+      this.buttonLabel = "Enrolled";
 
-    this.cartService.add(this.course._id).subscribe({
-      next: (response) => {
-        setTimeout(() => {
-          this.router.navigate(["course/enroll", this.course._id]);
-        }, 500);
-      },
-      error: (error) => {
-        // setTimeout(() => {
-        //   this.router.navigate(["shopping-cart"]);
-        // }, 500);
-      },
-    });
+      this.cartService.add(this.course._id).subscribe({
+        next: (response) => {
+          setTimeout(() => {
+            this.router.navigate(["course/enroll", this.course._id]);
+          }, 500);
+        },
+        error: (error) => {
+          // setTimeout(() => {
+          //   this.router.navigate(["shopping-cart"]);
+          // }, 500);
+        },
+      });
+    }
+    else{
+      setTimeout(() => {
+        this.router.navigate(["login"]);
+      }, 500);
+    }
+
   }
 }
