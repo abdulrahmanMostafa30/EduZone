3ad149336731f3dee71de3f013d9ad622485b051
abdulrahmanMostafa30 <3bdo.mostafa@protonmail.com>
2023-07-13 12:30:22 +0300

    edit API_URL

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import { baseEnvironment } from "./config";
 
 const environment = {
   PRODUCTION: true,
-  API_URL: "https://staging-dot-eduzone-391614.lm.r.appspot.com/",
+  API_URL: "https://staging-dot-eduzone-391614.lm.r.appspot.com",
   ...baseEnvironment,
 };
 
